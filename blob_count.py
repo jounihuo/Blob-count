3d9e955dfa6f915f15a173d8148a577d905568c0
Jouni Huopana <jhuopana@gmail.com>
2016-10-30 06:18:58 +0200

    Add files via upload
    First version.

---
+++ blob_count.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread 
+from skimage import color
+from skimage.feature import blob_dog
+from skimage.color import rgb2gray
+from skimage.draw import circle
+
+#Read image
+image = imread('barley.jpg')
+#Inverse the grayscale image
+image_gray = 1 - (rgb2gray(image))
+
+# Compute the blobs and estimate size
+blobs_dog = blob_dog(image_gray, max_sigma=30, threshold=.1)
+blobs_dog[:, 2] = blobs_dog[:, 2] * np.sqrt(2)
+
+#Plot image
+fig, ax = plt.subplots()
+ax.imshow(image)
+
+#Plot circles
+i=0
+for row in blobs_dog:
+    y, x, r = row
+    c = plt.Circle((x, y), r, color='lime', linewidth=2, fill=False)
+    ax.add_patch(c)
+    i=i+1
+    ax.annotate(str(i), xy=(2, 2), xytext=(x, y), color=(0.9,0,0.9))
+
+
+ax.annotate('Number of kernels = '+(str(i)), xy=(2, 2), xytext=(20, 40), color=(0.9,0,0.9))	
+ax.set_axis_off()
+plt.tight_layout()
+plt.show()
