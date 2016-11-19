78c335c77b5fdfdc77462a0cd372f28bb0a6cd0c
Jouni Huopana <Jouni Huopana>
2016-11-19 10:23:18 +0200

    Changed capture from image to video with high res.

--- blob_count.py
+++ blob_count.py
@@ -12,7 +12,7 @@ from matplotlib.backend_bases import key_press_handler
 # opencv
 # scikit-image
 import numpy as np
-from cv2 import *
+import cv2
 from skimage.io import imread, imsave 
 from skimage import color
 from skimage.feature import blob_dog
@@ -35,15 +35,8 @@ Difference of Gaussian (DoG) method is used to detetec the blobs. Blobs are assu
 root = Tk.Tk()
 root.wm_title("Counting things")
 
-#Capture an image from webcam
-# initialize the camera
-cam = VideoCapture(0)   # 0 -> index of camera
-s, img = cam.read()
-if s:
-    imsave('snap.jpg',img)
-
 #Read image
-image = imread('snap.jpg')
+image = cv2.imread('snap.jpg')
 #Plot image
 f = Figure(figsize=(6, 3), dpi=100)
 ax = f.add_subplot(111)
@@ -61,17 +54,56 @@ canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
 #Take a picture and count the blobs
 def _snap():
-    cam = VideoCapture(0)   # 0 -> index of camera
-    s, img = cam.read()
-    if s:
-        imsave('snap.jpg',img)
-        ax.clear()
-    image = imread('snap.jpg')
+    #cam = VideoCapture(2)   # 0 -> index of camera
+	# Camera 0 is the integrated web cam on my netbook
+    camera_port = 1
+
+    #Number of frames to throw away while the camera adjusts to light levels
+    ramp_frames = 30
+
+    # Now we can initialize the camera capture object with the cv2.VideoCapture class.
+    # All it needs is the index to a camera port.
+    camera = cv2.VideoCapture(camera_port)
+
+    camera.set(3,1920)
+    camera.set(4,1080)
+
+    # Captures a single image from the camera and returns it in PIL format
+    def get_image():
+        # read is the easiest way to get a full image out of a VideoCapture object.
+        retval, im = camera.read()
+        return im
+
+    # Ramp the camera - these frames will be discarded and are only used to allow v4l2
+    # to adjust light levels, if necessary
+    for i in xrange(ramp_frames):
+        temp = get_image()
+
+    print("Taking image...")
+    # Take the actual image we want to keep
+    camera_capture = get_image()
+    camera_capture = cv2.cvtColor(camera_capture, cv2.COLOR_BGR2GRAY)
+    file = "snap.jpg"
+    # A nice feature of the imwrite method is that it will automatically choose the
+    # correct format based on the file extension you provide. Convenient!
+    cv2.imwrite(file, camera_capture)
+    #cv2.imsave('snap.jpg',camera_capture)
+    # You'll want to release the camera, otherwise you won't be able to create a new
+    # capture object until your script exits
+    #del(camera)
+    #s, img = cam.read()
+    #if s:
+    #    imsave('snap.jpg',img)
+    #    ax.clear()
+    ax.clear()
+    image = cv2.imread('snap.jpg')
     ax.imshow(image)
 	#Inverse the grayscale image
-    image_gray = 1 - (rgb2gray(image))
+    #image_gray = rgb2gray(image)
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	#image_gray = 1 - (rgb2gray(image))
     #Compute the blobs and estimate size
-    blobs_dog = blob_dog(image_gray, max_sigma=30, threshold=w.get()/100.)
+    blobs_dog = blob_dog(image_gray, max_sigma=w2.get()/1., threshold=w.get()/200.)
     blobs_dog[:, 2] = blobs_dog[:, 2] * np.sqrt(2)
 	#Plot circles
     i=0
@@ -98,5 +130,9 @@ button_2.pack(side=Tk.BOTTOM)
 #Slider for threshold
 w = Tk.Scale(master=root, from_=1, to=100, label='Thershold')
 w.pack(side=Tk.BOTTOM)
+w.set(34)
+w2 = Tk.Scale(master=root, from_=1, to=50, label='sigma')
+w2.pack(side=Tk.BOTTOM)
+w2.set(7)
 
 Tk.mainloop()
